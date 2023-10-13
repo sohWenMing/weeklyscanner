8d07316708816cc791daf8502fd19688d8bb306d
Soh Wen Ming <wenming.soh@temus.com>
2023-10-13 08:46:13 +0800

    Untrack node_modules and update .gitignore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 const path = require("path");
 app.set('view engine', 'ejs')
+const PORT = process.env.PORT || 3000;
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -10,6 +11,6 @@ app.use(express.static(path.join(__dirname, "public")));
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
-app.listen(3000, () => {
-    console.log('getting something on port 3000');
+app.listen(PORT, () => {
+    console.log(`getting something on ${PORT}`);
 })
